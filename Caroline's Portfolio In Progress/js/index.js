1ca9fd7914c5809766caefdf54750f5fd4d4da8b
williamli2694@gmail.com <C.Win1121>
2017-02-04 17:12:51 -0500

    HomePage JS
    Update homepage JS

--- Caroline's Portfolio In Progress/js/index.js
+++ Caroline's Portfolio In Progress/js/index.js
@@ -1,40 +1,223 @@
 $(document).ready(function () {
-    
-    var designList = ["comingsoon", "carma"]
-    var designTitle = ["Capstone Project", "Carma"]
-    var designDesc = ["7 month research and development project", "Mobile app for tween social coordination"]
-    var designDate = ["Jan - Aug 2017", "Oct - Nov 2016"]
-    
-    var gameList = ["castleofdoors", "mindblock", "splashhour"]
-    var gameTitle = ["Castle of Doors", "Mindblock", "Splash Hour"]
-    var gameDesc = ["Training game for patients using prosthetic arms", "Educational game about emotional health", "Mobile game where you are the Jellyfish King!"]
-    var gameDate = ["Jan - Aug 2013", "Jan - Aug", "Jan - Aug"]
-    
-    var artList = ["graphicdesign", "animation", "sketches"]
-    var artTitle = ["Graphic Design", "2D and 3D Animation", "Sketches and Mixed Media"]
-    var artDesc = ["Aviation Management System", "Tween social coordination", "Type II Diabetes"]
-    
-    var count = 0
-    while (count < designList.length){
-        var designProject = '<a href="'+designList[count]+'.html"><div class = "projects"><img src="images/'+designList[count]+'.png"><p class ="projectTitle">'+designTitle[count]+'</p><p>'+designDesc[count]+'</p><p class = "imageCaption">'+designDate[count]+'</p></div></a>'
-        
-        $("#designWork").append(designProject)
-        count++
+
+    //Design section
+    var design = $("#designWork");
+    var designProjectCode = '<a href="%designLink%.html"><div class = "projects"><img src="images/%designImage%.png"><p class ="projectTitle">%designTitle%</p><p>%designDesc%</p><p class = "imageCaption">%designDate%</p></div></a>';
+
+    var designLink = "%designLink%";
+    var designImage = "%designImage%";
+    var designTitle = "%designTitle%";
+    var designDesc = "%designDesc%";
+    var designDates = "%designDate%";
+
+    var designProjects = {
+        "projects" : [{
+            "link" : "eaton",
+            "image" : "eaton",
+            "title" : "Eaton Dashboard",
+            "description" : "7 month research and development project",
+            "dates" : "Jan - Aug 2017"
+        }, {
+            "link" : "carma",
+            "image" : "carma",
+            "title" : "Carma",
+            "description" : "Mobile app for tween social coordination",
+            "dates" : "Oct - Nov 2016"
+        }, {
+            "link" : "avmosys",
+            "image" : "avmosys",
+            "title" : "AVMOSYS",
+            "description" : "Mobile app for business aviation management",
+            "dates" : "Nov 2015 - Nov 2016"
+        }, {
+            "link" : "capsule",
+            "image" : "capsule",
+            "title" : "Capsule",
+            "description" : "Digital platform for undergraduate student reflection",
+            "dates" : "Sept - Dec 2016"
+        }, {
+            "link" : "uresearch",
+            "image" : "uresearch",
+            "title" : "uResearch",
+            "description" : "Search and collaboration tools for academics",
+            "dates" : "Sept - Nov 2016"
+        }, {
+            "link" : "type2diabetes",
+            "image" : "type2diabetes",
+            "title" : "Type 2 Diabetes Education",
+            "description" : "Co-participatory design to improve lives of Type 2 diabetes patients",
+            "dates" : "Jan- May 2016"
+        }, {
+            "link" : "whatisux",
+            "image" : "whatisux",
+            "title" : "What is UX Design?",
+            "description" : "Website that explains UX design to non-designers",
+            "dates" : "Oct - Dec 2016"
+        }, {
+            "link" : "smartfit",
+            "image" : "smartfit",
+            "title" : "SmartFit",
+            "description" : "Mixed reality app that serves as a virtual trainer for gym members",
+            "dates" : "Nov - Dec 2016"
+        }, {
+            "link" : "scottychat",
+            "image" : "scottychat",
+            "title" : "ScottyChat",
+            "description" : "Mobile website that pairs CMU students with alumni mentors",
+            "dates" : "Sept - Oct 2016"
+        }, {
+            "link" : "dolcie",
+            "image" : "dolcie",
+            "title" : "DOLCIE",
+            "description" : "Type 1 Diabetes management app created in OH/IO Hackathon",
+            "dates" : "November 2015"
+        }, {
+            "link" : "virtualhealthcoach",
+            "image" : "virtualhealthcoach",
+            "title" : "Virtual Health Coach",
+            "description" : "Web app that guides patients in creating and reaching health goals",
+            "dates" : "May 2014 - Nov 2015"
+        }, {
+            "link" : "vitals",
+            "image" : "vitals",
+            "title" : "VITALS Dedesign",
+            "description" : "OSU medical student portal for tracking schedule and tasks",
+            "dates" : "October 2015"
+        }, {
+            "link" : "telemedicine",
+            "image" : "telemedicine",
+            "title" : "Telemedicine Website",
+            "description" : "Informational website for physician's virtual clinic",
+            "dates" : "Sept - Nov 2015"
+        }, {
+            "link" : "timebank",
+            "image" : "timebank",
+            "title" : "Timebank App",
+            "description" : "Mobile app that allows people to share skills & expertise",
+            "dates" : "June - Sept 2015"
+        }, {
+            "link" : "ridesouthla",
+            "image" : "ridesouthla",
+            "title" : "Ride South LA",
+            "description" : "Mobile event promotion strategy for non-profit in Los Angeles",
+            "dates" : "May - June 2012"
+        }]
+    };
+
+    designProjects.display = function() {
+        designProjects.projects.forEach(function(items) {
+            var newDesignProject = designProjectCode.replace(designLink, items.link).replace(designImage, items.image).replace(designTitle, items.title).replace(designDesc, items.description).replace(designDates, items.dates);
+            design.append(newDesignProject);
+        })
+    }
+
+    designProjects.display();
+
+    //Game section
+    var game = $("#gameWork");
+    var gameProjectCode = '<a href="%gameLink%.html"><div class = "projects"><img src="images/%gameImage%.png"><p class ="projectTitle">%gameTitle%</p><p>%gameDesc%</p><p class = "imageCaption">%gameDate%</p></div></a>';
+
+    var gameLink = "%gameLink%";
+    var gameImage = "%gameImage%";
+    var gameTitle = "%gameTitle%";
+    var gameDesc = "%gameDesc%";
+    var gameDates = "%gameDate%";
+
+    var gameProjects = {
+        "projects" : [{
+            "link" : "castleofdoors",
+            "image" : "castleofdoors",
+            "title" : "Castle of Doors",
+            "description" : "Training game for patients using prosthetic arms",
+            "dates" : "Jan - Aug 2013"
+        }, {
+            "link" : "mindblock",
+            "image" : "mindblock",
+            "title" : "Mindblock",
+            "description" : "Educational game about emotional health",
+            "dates" : "Jan - Aug 2013"
+        }, {
+            "link" : "splashhour",
+            "image" : "splashhour",
+            "title" : "Splash Hour",
+            "description" : "Mobile game where you are the Jellyfish King!",
+            "dates" : "Jan - Aug 2013"
+        }]
+    };
+
+    gameProjects.display = function() {
+        gameProjects.projects.forEach(function(items) {
+            var newGameProject = gameProjectCode.replace(gameLink, items.link).replace(gameImage, items.image).replace(gameTitle, items.title).replace(gameDesc, items.description).replace(gameDates, items.dates);
+            game.append(newGameProject);
+        })
     }
     
-    count = 0
-    while (count < gameList.length){
-        var gameProject = '<a href="'+gameList[count]+'.html"><div class = "projects"><img src="images/'+gameList[count]+'.png"><p class ="projectTitle">'+gameTitle[count]+'</p><p>'+gameDesc[count]+'</p><p class = "imageCaption">'+gameDate[count]+'</p></div></a>'
-        
-        $("#gameWork").append(gameProject)
-        count++
-    }
     
-    count = 0
-    while (count < artList.length){
-        var artProject = '<a href="'+artList[count]+'.html"><div class = "projects"><img src="images/'+artList[count]+'.png"><p class ="projectTitle">'+artTitle[count]+'</p><p>'+artDesc[count]+'</p></div></a>'
-        
-        $("#artWork").append(artProject)
-        count++
+    //Art section
+    var art = $("#artWork");
+    var artProjectCode = '<a href="%artLink%.html"><div class = "projects"><img src="images/%artImage%.png"><p class ="projectTitle">%artTitle%</p><p>%artDesc%</p></div></a>';
+
+    var artLink = "%artLink%";
+    var artImage = "%artImage%";
+    var artTitle = "%artTitle%";
+    var artDesc = "%artDesc%";
+    var artDates = "%artDate%";
+
+    var artProjects = {
+        "projects" : [{
+            "link" : "graphicdesign",
+            "image" : "graphicdesign",
+            "title" : "Graphic Design",
+            "description" : "TODO",
+        }, {
+            "link" : "animation",
+            "image" : "animation",
+            "title" : "2D and 3D Animation",
+            "description" : "TODO",
+        }, {
+            "link" : "sketches",
+            "image" : "sketches",
+            "title" : "Sketches and Mixed Media",
+            "description" : "TODO",
+        }]
+    };
+
+    artProjects.display = function() {
+        artProjects.projects.forEach(function(items) {
+            var newArtProject = artProjectCode.replace(artLink, items.link).replace(artImage, items.image).replace(artTitle, items.title).replace(artDesc, items.description).replace(artDates, items.dates);
+            art.append(newArtProject);
+        })
     }
+
+    artProjects.display();
+    
+
+    gameProjects.display();
+    //    var artList = ["graphicdesign", "animation", "sketches"]
+    //    var artTitle = ["Graphic Design", "2D and 3D Animation", "Sketches and Mixed Media"]
+    //    var artDesc = ["Aviation Management System", "Tween social coordination", "Type II Diabetes"]
+    //    
+    //    var count = 0
+    //    while (count < designList.length){
+    //        var designProject = '<a href="'+designList[count]+'.html"><div class = "projects"><img src="images/'+designList[count]+'.png"><p class ="projectTitle">'+designTitle[count]+'</p><p>'+designDesc[count]+'</p><p class = "imageCaption">'+designDate[count]+'</p></div></a>'
+    //        
+    //        $("#designWork").append(designProject)
+    //        count++
+    //    }
+    //    
+    //    count = 0
+    //    while (count < gameList.length){
+    //        var gameProject = '<a href="'+gameList[count]+'.html"><div class = "projects"><img src="images/'+gameList[count]+'.png"><p class ="projectTitle">'+gameTitle[count]+'</p><p>'+gameDesc[count]+'</p><p class = "imageCaption">'+gameDate[count]+'</p></div></a>'
+    //        
+    //        $("#gameWork").append(gameProject)
+    //        count++
+    //    }
+    //    
+    //    count = 0
+    //    while (count < artList.length){
+    //        var artProject = '<a href="'+artList[count]+'.html"><div class = "projects"><img src="images/'+artList[count]+'.png"><p class ="projectTitle">'+artTitle[count]+'</p><p>'+artDesc[count]+'</p></div></a>'
+    //        
+    //        $("#artWork").append(artProject)
+    //        count++
+    //    }
 })
