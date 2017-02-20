7f7af8dc4e00b0c02fedca5143383c58f16acce4
wincaroline <wincaroline@gmail.com>
2017-02-19 21:19:43 -0500

    Updated dropdown menu styling and header background image
    changed url to “../images/header_waves.svg”

--- Caroline's Portfolio In Progress/js/common.js
+++ Caroline's Portfolio In Progress/js/common.js
@@ -2,11 +2,45 @@ $(document).ready(function () {
 
     var fullHeader = '<div id = "headerContainerFull"><div id = "iconContainer"><a href = "#"><img class = "iconsHeader" src="images/twitter.svg"></a><a href = "#"><img class = "iconsHeader" src="images/linkedin.svg"></a><a href = "#"><img class = "iconsHeader" src="images/instagram.svg"></a></div><div id = "logoContainer"><a href = "index.html"><div><p id = "logo">Caroline Win</p><p id = "sub-logo">UX &#38; INTERACTION DESIGNER</p></div></a></div><nav><ul id = "navBar"> <!-- nav items need to be in backwards order due to have right set to 0px; --><li class = "primaryNav"><a class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav></div>'
 
-    var collapsedHeader = '<div id="headerwords"><div id="logo"><a href="index.html"></a></div><ul id="navtop"></ul></div><div id = "headerContainerCollapsed"><a href = "index.html"><div id = "collapsedLogoContainer"><h1 id = "logo"><p id = "logo">Caroline Win</p></h1></div></a><nav><ul id = "navBarCollapsed"><li class = "primaryNav"><a class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav>'
+    /* var collapsedHeader = '<div id="headerwords"><div id="logo"><a href="index.html"></a></div><ul id="navtop"></ul></div><div id = "headerContainerCollapsed"><a href = "index.html"><div id = "collapsedLogoContainer"><h1 id = "logo"><p id = "logo">Caroline Win</p></h1></div></a><nav><ul id = "navBarCollapsed"><li class = "primaryNav"><a class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav>' */
+
+    var collapsedHeader = '<div id="headerwords">' +
+      '<div id="logo">' +
+        '<a href="index.html"></a>' +
+      '</div>' +
+      '<ul id="navtop"></ul>' +
+    '</div>' +
+
+    '<div id = "headerContainerCollapsed">' +
+
+      '<a href = "index.html">' +
+        '<div id = "collapsedLogoContainer">' +
+          '<h1 id = "logo">Caroline Win</h1>' +
+        '</div>' +
+      '</a>' +
+
+      '<nav>' +
+        '<ul id = "navBarCollapsed">' +
+          '<li class = "primaryNav"><a class = "primaryNavItems" href = "contact.html"> CONTACT</a></li>' +
+          '<li class = "primaryNav"><a class = "primaryNavItems" href = "about.html"> ABOUT </a></li>' +
+          '<li class = "primaryNav"><a class = "primaryNavItems" href = "resume.html"> RESUME </a></li>' +
+          '<li class = "primaryNav"><a class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li>' +
+        '</ul>' +
+      '</nav>' +
+
+    '</div>'
 
     var footer = '<div id = "backToTopContainer"><div id = "backToTop"><a href = "#">&uarr; Back to Top</a></div></div><div id = "footerContainer"><div id = "footer"><p id = "copyright">© 2017 Caroline Win</p></div></div>'
 
-    var dropdown = '<div class="dropdown"><button class="dropbtn">Menu</button><div class="dropdown-content"><a href="#">Portfolio</a><a href="#">Resume</a><a href="#">About</a><a href="#">Contanct</a></div></div>'
+    var dropdown = '<div class="dropdown">' +
+        '<button class="dropbtn">MENU</button>' +
+        '<div class="dropdown-content">' +
+          '<a href="#">PORTFOLIO</a>' +
+          '<a href="#">RESUME</a>' +
+          '<a href="#">ABOUT</a>' +
+          '<a href="#">CONTACT</a>' +
+        '</div>' +
+      '</div>'
 
     $("header").append(fullHeader)
     $("header").append(collapsedHeader)
