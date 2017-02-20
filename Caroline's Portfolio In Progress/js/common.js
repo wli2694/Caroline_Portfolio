061c66133d902f894c5b2466e055c0c02c583f81
williamli2694@gmail.com <C.Win1121>
2017-02-19 22:57:18 -0500

    Highlighting
    Highlight navBar items when on corresponding page.

--- Caroline's Portfolio In Progress/js/common.js
+++ Caroline's Portfolio In Progress/js/common.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
 
-    var fullHeader = '<div id = "headerContainerFull"><div id = "iconContainer"><a href = "#"><img class = "iconsHeader" src="images/twitter.svg"></a><a href = "#"><img class = "iconsHeader" src="images/linkedin.svg"></a><a href = "#"><img class = "iconsHeader" src="images/instagram.svg"></a></div><div id = "logoContainer"><a href = "index.html"><div><h1 id = "logo">Caroline Win</h1><h2 id = "sub-logo">UX &#38; INTERACTION DESIGNER</h2></div></a></div><nav><ul id = "navBar"> <!-- nav items need to be in backwards order due to have right set to 0px; --><li class = "primaryNav"><a class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav></div>'
+    var fullHeader = '<div id = "headerContainerFull"><div id = "iconContainer"><a href = "#"><img class = "iconsHeader" src="images/twitter.svg"></a><a href = "#"><img class = "iconsHeader" src="images/linkedin.svg"></a><a href = "#"><img class = "iconsHeader" src="images/instagram.svg"></a></div><div id = "logoContainer"><a href = "index.html"><div><h1 id = "logo">Caroline Win</h1><h2 id = "sub-logo">UX &#38; INTERACTION DESIGNER</h2></div></a></div><nav><ul id = "navBar"> <!-- nav items need to be in backwards order due to have right set to 0px; --><li class = "primaryNav"><a id = "contactNav" class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a id = "aboutNav" class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a id = "resumeNav" class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a id = "portfolioNav" class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav></div>'
 
-    var collapsedHeader = '<div id="collapsedBackground"><div id = "headerContainerCollapsed"><a href = "index.html"><div id = "collapsedLogoContainer"><h1 id = "logo">Caroline Win</h1></div></a><nav><ul id = "navBarCollapsed"><li class = "primaryNav"><a class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav></div></div>'
+    var collapsedHeader = '<div id="collapsedBackground"><div id = "headerContainerCollapsed"><a href = "index.html"><div id = "collapsedLogoContainer"><h1 id = "logo">Caroline Win</h1></div></a><nav><ul id = "navBarCollapsed"><li class = "primaryNav"><a id = "contactNavC" class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a id = "aboutNavC" class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a id = "resumeNavC" class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a id = "portfolioNavC" class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav></div></div>'
 
     var footer = '<div id = "backToTopContainer"><div id="backToTop"><a href = "#">&uarr; Back to Top</a></div></div><div id = "footerContainer"><div id = "footer"><p id = "copyright">© 2017 Caroline Win</p></div></div>'
 
     var dropdown = '<div class="dropdown">' +
         '<button class="dropbtn">MENU</button>' +
         '<div class="dropdown-content">' +
-            '<a href="#">PORTFOLIO</a>' +
-            '<a href="#">RESUME</a>' +
-            '<a href="#">ABOUT</a>' +
-            '<a href="#">CONTACT</a>' +
+        '<a href="#">PORTFOLIO</a>' +
+        '<a href="#">RESUME</a>' +
+        '<a href="#">ABOUT</a>' +
+        '<a href="#">CONTACT</a>' +
         '</div>' +
-    '</div>'
+        '</div>'
 
     $("header").append(fullHeader)
     $("header").append(collapsedHeader)
@@ -45,7 +45,6 @@ $(document).ready(function () {
 
         $(window).bind('resize', function() {
             var windowwidth = $(window).width();
-            console.log(windowwidth);
             if(windowwidth >= 1200){
                 $("#sub-logo").show();
                 $("#iconContainer").show();
@@ -105,17 +104,47 @@ $(document).ready(function () {
         return false;
     });
 
-    if(window.location.href === '../resume.html') {
+    
+    if($(this).attr('title') == 'Caroline Win | Resume') {
+        $("#resumeNav").addClass("highlight");
+        $("#resumeNavC").addClass("highlight");
+        $("#portfolioNav").removeClass("highlight");
+        $("#aboutNav").removeClass("highlight");
+        $("#contactNav").removeClass("highlight");
+        $("#portfolioNavC").removeClass("highlight");
+        $("#aboutNavC").removeClass("highlight");
+        $("#contactNavC").removeClass("highlight");
 
     }
-    else if(window.location.href === '../about.html') {
-
+    else if($(this).attr('title') == 'Caroline Win | About') {
+        $("#aboutNav").addClass("highlight");
+        $("#aboutNavC").addClass("highlight");
+        $("#portfolioNav").removeClass("highlight");
+        $("#contactNav").removeClass("highlight");
+        $("#resumeNav").removeClass("highlight");
+         $("#portfolioNavC").removeClass("highlight");
+        $("#contactNavC").removeClass("highlight");
+        $("#resumeNavC").removeClass("highlight");
     }
-    else if(window.location.href === '../contact.html') {
-
+    else if($(this).attr('title') == 'Caroline Win | Contact Me') {
+        $("#contactNav").addClass("highlight");
+        $("#contactNavC").addClass("highlight");
+        $("#portfolioNav").removeClass("highlight");
+        $("#aboutNav").removeClass("highlight");
+        $("#resumeNav").removeClass("highlight");
+        $("#portfolioNavC").removeClass("highlight");
+        $("#aboutNavC").removeClass("highlight");
+        $("#resumeNavC").removeClass("highlight");
     }
     else {
-
+        $("#portfolioNav").addClass("highlight");
+        $("#portfolioNavC").addClass("highlight");
+        $("#aboutNav").removeClass("highlight");
+        $("#contactNav").removeClass("highlight");
+        $("#resumeNav").removeClass("highlight");
+        $("#aboutNavC").removeClass("highlight");
+        $("#contactNavC").removeClass("highlight");
+        $("#resumeNavC").removeClass("highlight");
     }
 
 })
