135ccd49103d7754dc67e43323110f4805ed6137
wincaroline <wincaroline@gmail.com>
2017-02-19 22:14:56 -0500

    Added smoother transitions for hovering
    Also made to-do list

--- Caroline's Portfolio In Progress/js/common.js
+++ Caroline's Portfolio In Progress/js/common.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 
     var fullHeader = '<div id = "headerContainerFull"><div id = "iconContainer"><a href = "#"><img class = "iconsHeader" src="images/twitter.svg"></a><a href = "#"><img class = "iconsHeader" src="images/linkedin.svg"></a><a href = "#"><img class = "iconsHeader" src="images/instagram.svg"></a></div><div id = "logoContainer"><a href = "index.html"><div><p id = "logo">Caroline Win</p><p id = "sub-logo">UX &#38; INTERACTION DESIGNER</p></div></a></div><nav><ul id = "navBar"> <!-- nav items need to be in backwards order due to have right set to 0px; --><li class = "primaryNav"><a class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav></div>'
 
-    /* var collapsedHeader = '<div id="headerwords"><div id="logo"><a href="index.html"></a></div><ul id="navtop"></ul></div><div id = "headerContainerCollapsed"><a href = "index.html"><div id = "collapsedLogoContainer"><h1 id = "logo"><p id = "logo">Caroline Win</p></h1></div></a><nav><ul id = "navBarCollapsed"><li class = "primaryNav"><a class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav>' */
+    var collapsedHeader = '<div id="headerwords"><div id="logo"><a href="index.html"></a></div><ul id="navtop"></ul></div><div id = "headerContainerCollapsed"><a href = "index.html"><div id = "collapsedLogoContainer"><h1 id = "logo"><p id = "logo">Caroline Win</p></h1></div></a><nav><ul id = "navBarCollapsed"><li class = "primaryNav"><a class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav></div>'
 
-    var collapsedHeader = '<div id="headerwords">' +
+    /* var collapsedHeader = '<div id="headerwords">' +
       '<div id="logo">' +
         '<a href="index.html"></a>' +
       '</div>' +
@@ -28,9 +28,9 @@ $(document).ready(function () {
         '</ul>' +
       '</nav>' +
 
-    '</div>'
+    '</div>' */
 
-    var footer = '<div id = "backToTopContainer"><div id = "backToTop"><a href = "#">&uarr; Back to Top</a></div></div><div id = "footerContainer"><div id = "footer"><p id = "copyright">© 2017 Caroline Win</p></div></div>'
+    var footer = '<div id = "backToTopContainer"><div id="backToTop"><a href = "#">&uarr; Back to Top</a></div></div><div id = "footerContainer"><div id = "footer"><p id = "copyright">© 2017 Caroline Win</p></div></div>'
 
     var dropdown = '<div class="dropdown">' +
         '<button class="dropbtn">MENU</button>' +
@@ -92,4 +92,23 @@ $(document).ready(function () {
             }
         })
     })
+
+    $('#backToTop').click(function() {
+      $('html, body').animate({scrollTop : 0}, 800);
+      return false;
+    });
+
+    if(window.location.href === '../resume.html') {
+
+    }
+    else if(window.location.href === '../about.html') {
+
+    }
+    else if(window.location.href === '../contact.html') {
+
+    }
+    else {
+
+    }
+    
 })
