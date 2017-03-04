aac2b660afeb3b7302fcd8710196c35c4b5095be
williamli2694@gmail.com <C.Win1121>
2017-03-04 00:27:25 -0500

    Header for static screen
    Fixed spacing and alignment for header/menu/nav when viewing in
    different viewports. Still need to fix resizing.

--- Caroline's Portfolio In Progress/js/common.js
+++ Caroline's Portfolio In Progress/js/common.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     var fullHeader = '<div id = "headerContainerFull"><div id = "iconContainer"><a href = "#"><img class = "iconsHeader" src="images/twitter.svg"></a><a href = "#"><img class = "iconsHeader" src="images/linkedin.svg"></a><a href = "#"><img class = "iconsHeader" src="images/instagram.svg"></a></div><div id = "logoContainer"><a href = "index.html"><div><h1 id = "logo">Caroline Win</h1><h2 id = "sub-logo">UX &#38; INTERACTION DESIGNER</h2></div></a></div><nav><ul id = "navBar"> <!-- nav items need to be in backwards order due to have right set to 0px; --><li class = "primaryNav"><a id = "contactNav" class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a id = "aboutNav" class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a id = "resumeNav" class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a id = "portfolioNav" class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav></div>'
 
-    var collapsedHeader = '<div id="collapsedBackground"><div id = "headerContainerCollapsed"><a href = "index.html"><div id = "collapsedLogoContainer"><h1 id = "logo">Caroline Win</h1></div></a><nav><ul id = "navBarCollapsed"><li class = "primaryNav"><a id = "contactNavC" class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a id = "aboutNavC" class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a id = "resumeNavC" class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a id = "portfolioNavC" class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav></div></div><div id="drop2"></div>'
+    var collapsedHeader = '<div id="collapsedBackground"><div id = "headerContainerCollapsed"><a href = "index.html"><div id = "collapsedLogoContainer"><h1 id = "logo">Caroline Win</h1></div></a><nav><ul id = "navBarCollapsed"><li class = "primaryNav"><a id = "contactNavC" class = "primaryNavItems" href = "contact.html"> CONTACT</a></li><li class = "primaryNav"><a id = "aboutNavC" class = "primaryNavItems" href = "about.html"> ABOUT </a></li><li class = "primaryNav"><a id = "resumeNavC" class = "primaryNavItems" href = "resume.html"> RESUME </a></li><li class = "primaryNav"><a id = "portfolioNavC" class = "primaryNavItems" href = "index.html"> PORTFOLIO </a></li></ul></nav></div></div>'
 
     var footer = '<div id = "backToTopContainer"><div id="backToTop"><a href = "#">&uarr; Back to Top</a></div></div><div id = "footerContainer"><div id = "footer"><p id = "copyright">© 2017 Caroline Win</p></div></div>'
 
@@ -20,14 +20,14 @@ $(document).ready(function () {
     $("header").append(fullHeader)
     $("header").append(collapsedHeader)
     $("#drop").append(dropdown)
-    //    $("#drop2").append(dropdown)
     $("footer").append(footer)
 
-    var startingWidth = $(window).width();
 
     //Vertical Responsiveness
     //Starting screen
     $(function() {
+        var startingWidth = $(window).width();
+
         $("#collapsedBackground").hide();
         $(".dropdown").hide();
         if(startingWidth <= 800){
@@ -61,27 +61,30 @@ $(document).ready(function () {
             if( scroll >= collapseHeaderDistance){
                 $("#collapsedBackground").show();
                 $("#headerContainerFull").hide();
-                
+
                 //full width dropdown
-                if($(window).width < 450){
+                if($(window).width() < 450){
                     $("#collapsedLogoContainer").hide();
-                    $("#drop").css({"position":"fixed", "width":"100%","height":"100px", "right":"0px", "top":"15px"});
+                    $("#drop").css({"position":"fixed", "width":"100%","height":"100px", "right":"0px", "top":"9px", "margin-right":"0px"});
                     $(".dropdown").css("width","100%");
+                    $(".projects").css("width","calc(100% - 32px)");
+                } else if($(window).width() <800) {
+                    $("#drop").css({"position":"fixed", "width":"100%","height":"100px", "right":"0px", "top":"9px"});
+                    $("#collapsedLogoContainer").css("top", "24px");
                 }
                 /*Full Header*/
             } else {
                 $("#collapsedBackground").hide();
                 $("#headerContainerFull").show();
-
-                if(collapseHeaderDistance <=250){
-                    if($(window).width() < 450){
-                        $("#drop").css({"position":"relative", "width":"","height":"", "right":"", "top":""});
-                        $(".dropdown").css("width","100%");
-                    } else {
-                        $("#drop").css({"position":"relative", "width":"","height":"", "right":"", "top":""});
-                        $(".dropdown").css("width","30%");
-                    }
+                if($(window).width() < 450){
+                    $("#drop").css({"position":"relative", "width":"","height":"", "right":"", "top":"", "margin-right":"0px"});
+                    $(".dropdown").css("width","100%");
+                    $(".projects").css("width","calc(100% - 32px)");
+                } else {
+                    $("#drop").css({"position":"relative", "width":"","height":"", "right":"", "top":""});
+                    $(".dropdown").css("width","30%");
                 }
+
             }
         })
 
@@ -114,6 +117,8 @@ $(document).ready(function () {
                 $(".dropdown").show();
 
                 if(windowwidth < 450){
+                    $("#logoContainer").css("width", "100%");
+                    $("#logo").css({"display":"", "text-align":"center"});
                     if((window.pageYOffset || document.documentElement.scrollTop) < 250){
                         $("#drop").css({"position":"relative", "width":"","height":"", "right":"", "top":""});
                         $(".dropdown").css("width","100%");
@@ -154,6 +159,15 @@ $(document).ready(function () {
             $("#navBar").hide();
             $("#navBarCollapsed").hide();
             $(".dropdown").show();
+
+            if(startingWidth < 450){
+                $("#logoContainer").css("width", "100%");
+                $("#logo").css({"display":"block", "text-align":"center"});
+                $(".projects").css("width","calc(100% - 32px)")
+                $("#drop").css("margin-right","0px")
+            } else {
+                $("#logoContainer").css("bottom","32px");
+            }
 
             collapseHeaderDistance = 250;
             collapseHeaderFunction();
